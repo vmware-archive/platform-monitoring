a87932b354c4c43b1d454a7fedb546449be55007
Robert Sullivan <rsullivan@pivotal.io>
2017-02-27 16:40:51 -0700

    ignore host key checking
    Signed-off-by: Kira Combs <kcombs@pivotal.io>

--- kpi_monitoring/run_kill_netcat.sh
+++ kpi_monitoring/run_kill_netcat.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ssh -i $OPSMANAGER_KEYPATH $OPSMANAGER_USERNAME@$OPSMANAGER_URL'bash -s' < "sudo lsof -t -i tcp:12345 -s tcp:listen | sudo xargs kill"
+ssh -o "StrictHostKeyChecking no" -i $OPSMANAGER_KEYPATH $OPSMANAGER_USERNAME@$OPSMANAGER_URL'bash -s' < "sudo lsof -t -i tcp:12345 -s tcp:listen | sudo xargs kill"
