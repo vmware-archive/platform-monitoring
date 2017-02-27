7c90cb03a970e577234eab8c69e7b3fbe0447d87
Robert Sullivan <rsullivan@pivotal.io>
2017-02-27 16:35:50 -0700

    fix run netcat
    Signed-off-by: Kira Combs <kcombs@pivotal.io>

--- kpi_monitoring/run_slow_netcat.sh
+++ kpi_monitoring/run_slow_netcat.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ssh -i $OPSMANAGER_KEYPATH $OPSMANAGER_USERNAME@$OPSMANAGER_URL'bash -s' < assets/slow_netcat
+ssh -i $OPSMANAGER_KEYPATH $OPSMANAGER_USERNAME@$OPSMANAGER_URL 'bash -s' < assets/slow_netcat
