81a5d6c92229c2c88f984d1426270ce6a73ab1e3
Robert Sullivan <rsullivan@pivotal.io>
2017-03-02 09:08:03 -0700

    make run time configurable
    Signed-off-by: Kira Combs <kcombs@pivotal.io>

--- kpi_monitoring/runValidator.sh
+++ kpi_monitoring/runValidator.sh
@@ -8,6 +8,6 @@ java -DHOSTNAME=$PROVIDER_IP \
      -DPASSWORD=$PROVIDER_PASSWORD \
      -DCF_DEPLOYMENT_NAME=cf \
      -DNOZZLE_PREFIX=opentsdb.nozzle. \
-     -DRUN_TIME_MINUTES=5 \
+     -DRUN_TIME_MINUTES=$RUN_TIME_MINUTES \
      -DPOLL_INTERVAL_SECONDS=5 \
     -jar target/platform-monitoring-validator-1.1-jar-with-dependencies.jar
