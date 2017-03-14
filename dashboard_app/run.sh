cd5ed8863e9663c59f187572a10191809944e5c0
Jean de Klerk <jadekler@gmail.com>
2017-03-14 11:54:49 -0600

    Initial ui controller setup
    Signed-off-by: Jeanette Booher <jbooher-lasell@pivotal.io>

--- dashboard_app/run.sh
+++ dashboard_app/run.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 export JMX_USERNAME=root
 export JMX_PASSWORD=root
 export JMX_SERVICE_URL="service:jmx:rmi://35.185.60.162:44445/jndi/rmi://35.185.60.162:44444/jmxrmi"
--- dashboard_ui/src/main/java/io/pivotal/plaformMonitoring/MetricsController.java
+++ dashboard_ui/src/main/java/io/pivotal/plaformMonitoring/MetricsController.java
@@ -1,15 +1,13 @@
 package io.pivotal.plaformMonitoring;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MetricsController {
-    @RequestMapping(method = RequestMethod.GET, value = "/")
-    @ResponseBody
-    public String firehoseLossRate() {
-        return "<html>I am a ui</html>";
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String getIndex() {
+        return "home";
     }
 }
