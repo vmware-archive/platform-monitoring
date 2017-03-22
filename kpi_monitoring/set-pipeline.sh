44b0c69429965654c29327849a3749dc751d59ee
Jean de Klerk <jadekler@gmail.com>
2017-03-22 11:18:25 -0600

    Run latest cf deployment before kpi validator in ci [#141979289]

--- kpi_monitoring/set-pipeline.sh
+++ kpi_monitoring/set-pipeline.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set +e
 
+if [ -z "$1" ]
+  then
+    echo "usage ./set-pipeline cf | opsman | redeploy-cf"
+    exit
+fi
+
 lpass ls > /dev/null # check that we're logged in
 
-fly --target concourse set-pipeline -p platform-monitoring --config pipeline.yml \
+fly --target concourse set-pipeline -p kpi-validator --config pipeline-$1.yml \
     --load-vars-from <(lpass show --notes "Shared-apm/concourse/credentials.yml") \
     --load-vars-from <(lpass show --notes "Shared-apm/concourse/kpi-validator.yml")
