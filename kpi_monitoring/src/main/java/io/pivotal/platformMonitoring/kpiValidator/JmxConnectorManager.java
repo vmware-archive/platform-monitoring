698ac622e7fbae3271f86f10ccfe6bda6a0204b4
Jean de Klerk <jadekler@gmail.com>
2017-03-22 13:20:44 -0600

    Refactor kpi validator for streams/readability, change receivedEnvelopes to totalReceivedMessageCount
    [#142181819]

---
+++ kpi_monitoring/src/main/java/io/pivotal/platformMonitoring/kpiValidator/JmxConnectorManager.java
@@ -0,0 +1,46 @@
+package io.pivotal.platformMonitoring.kpiValidator;
+
+import javax.management.remote.JMXConnector;
+import javax.management.remote.JMXConnectorFactory;
+import javax.management.remote.JMXServiceURL;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JmxConnectorManager {
+    private static final String HOSTNAME = System.getProperty("HOSTNAME", "localhost:44444");
+    private static final String USERNAME = System.getProperty("USERNAME", "admin");
+    private static final String PASSWORD = System.getProperty("PASSWORD", "password");
+    private static final String CF_DEPLOYMENT_NAME = System.getProperty("CF_DEPLOYMENT_NAME", "cf");
+    private static final String NOZZLE_PREFIX = System.getProperty("NOZZLE_PREFIX", "opentsdb.nozzle.");
+    private static final int RUN_TIME = new Integer(System.getProperty("RUN_TIME_MINUTES", "5"));
+    private static final int POLL_INTERVAL = new Integer(System.getProperty("POLL_INTERVAL_SECONDS", "5"));
+
+    private static JMXConnector conn;
+
+    public static JMXConnector getConnection() throws IOException {
+        if(JmxConnectorManager.conn != null) {
+            return JmxConnectorManager.conn;
+        }
+
+        System.out.println("HOSTNAME: " + HOSTNAME);
+        System.out.println("CF_DEPLOYMENT_NAME: " + CF_DEPLOYMENT_NAME);
+        System.out.println("NOZZLE_PREFIX: " + NOZZLE_PREFIX);
+        System.out.println("RUN_TIME_MINUTES: " + RUN_TIME);
+        System.out.println("POLL_INTERVAL_SECONDS: " + POLL_INTERVAL);
+
+        JMXServiceURL serviceURL = new JMXServiceURL("service:jmx:rmi://" + HOSTNAME + ":44445/jndi/rmi://" + HOSTNAME + ":44444/jmxrmi");
+        String[] creds = new String[]{USERNAME, PASSWORD};
+        Map<String, String[]> env = new HashMap<>();
+        env.put(JMXConnector.CREDENTIALS, creds);
+
+        JmxConnectorManager.conn = JMXConnectorFactory.connect(serviceURL, env);
+        return conn;
+    }
+
+    public static void closeConnection() throws IOException {
+        if(JmxConnectorManager.conn != null) {
+            JmxConnectorManager.conn.close();
+        }
+    }
+}
--- kpi_monitoring/src/main/java/io/pivotal/platformMonitoring/kpiValidator/Validator.java
+++ kpi_monitoring/src/main/java/io/pivotal/platformMonitoring/kpiValidator/Validator.java
@@ -1,127 +1,94 @@
 package io.pivotal.platformMonitoring.kpiValidator;
 
-import javax.management.MBeanAttributeInfo;
 import javax.management.MBeanServerConnection;
 import javax.management.ObjectName;
 import javax.management.remote.JMXConnector;
-import javax.management.remote.JMXConnectorFactory;
-import javax.management.remote.JMXServiceURL;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
-import java.util.stream.Stream;
+
+import static io.pivotal.platformMonitoring.kpiValidator.JmxConnectorManager.closeConnection;
+import static io.pivotal.platformMonitoring.kpiValidator.JmxConnectorManager.getConnection;
+import static java.util.stream.Collectors.toList;
 
 public class Validator {
-    private static final String HOSTNAME = System.getProperty("HOSTNAME", "localhost:44444");
-    private static final String USERNAME = System.getProperty("USERNAME", "admin");
-    private static final String PASSWORD = System.getProperty("PASSWORD", "password");
     private static final String CF_DEPLOYMENT_NAME = System.getProperty("CF_DEPLOYMENT_NAME", "cf");
     private static final String NOZZLE_PREFIX = System.getProperty("NOZZLE_PREFIX", "opentsdb.nozzle.");
-    private static final int RUN_TIME = new Integer(System.getProperty("RUN_TIME_MINUTES", "5"));
-    private static final int POLL_INTERVAL = new Integer(System.getProperty("POLL_INTERVAL_SECONDS", "5"));
-
-    private JMXConnector jmxConnector;
+    private static final int RUN_TIME_MINUTES = new Integer(System.getProperty("RUN_TIME_MINUTES", "5"));
+    private static final int POLL_INTERVAL_SECONDS = new Integer(System.getProperty("POLL_INTERVAL_SECONDS", "5"));
+    private static final String QUERY_NAME = String.format("*:deployment=%1s*,job=*,index=*,ip=*,*", CF_DEPLOYMENT_NAME);
+    private static final String KPI_FILE_NAME = "kpis.txt";
 
     public static void main(String[] args) {
-        System.out.println("Running Validator");
-        Validator validator = new Validator();
+        System.out.println("Running validator");
+
         try {
-            validator.run();
+            Validator.run();
         } catch(Exception e) {
             e.printStackTrace();
             System.exit(1);
         }
     }
 
-    public void run() throws Exception {
-        createConnection();
-        Set<String> kpis = readMetrics();
+    private static void run() throws Exception {
+        JMXConnector conn = getConnection();
+        MBeanServerConnection mbeanConn = conn.getMBeanServerConnection();
+
         Set<String> receivedMetrics = new HashSet<>();
         Set<String> fullMetrics = new HashSet<>();
-        MBeanServerConnection mbeanConn = jmxConnector.getMBeanServerConnection();
-        String queryName = String.format("*:deployment=%1s*,job=*,index=*,ip=*,*", CF_DEPLOYMENT_NAME);
+
         System.out.println("Started Capturing Metrics: " + Calendar.getInstance().getTime());
-        for(int i = 0; i < RUN_TIME * 60 / POLL_INTERVAL; i++) {
-            Set<ObjectName> names = mbeanConn.queryNames(new ObjectName(queryName), null);
+
+        for(int i = 0; i < RUN_TIME_MINUTES * 60 / POLL_INTERVAL_SECONDS; i++) {
+            Set<ObjectName> names = mbeanConn.queryNames(new ObjectName(QUERY_NAME), null);
             for(ObjectName name : names) {
-                for(MBeanAttributeInfo attr : mbeanConn.getMBeanInfo(name).getAttributes()) {
-                    receivedMetrics.add(attr.getName().replaceAll(NOZZLE_PREFIX, ""));
-                    fullMetrics.add(name + ":" + attr.getName().replaceAll(NOZZLE_PREFIX, ""));
-                }
+                Arrays.stream(mbeanConn.getMBeanInfo(name).getAttributes())
+                    .map(attr -> attr.getName().replaceAll(NOZZLE_PREFIX, ""))
+                    .peek(receivedMetrics::add)
+                    .map(attr -> String.format("%s:%s", name, attr))
+                    .forEach(fullMetrics::add);
             }
-            Thread.sleep(POLL_INTERVAL * 1000);
+
+            Thread.sleep(POLL_INTERVAL_SECONDS * 1000);
         }
+
         System.out.println("Stopped Capturing Metrics: " + Calendar.getInstance().getTime());
-        System.out.println("Received " + receivedMetrics.size() + " metrics.");
+        System.out.println(String.format("Received %d metrics.", receivedMetrics.size()));
         closeConnection();
 
-        System.out.println("*********************************RECEIVED METRICS***************************");
-        List<String> sortedMetrics = new ArrayList<String>();
-        sortedMetrics.addAll(fullMetrics);
-        Collections.sort(sortedMetrics);
-        for(String metric : sortedMetrics) {
-            System.out.println(metric);
-        }
-        System.out.println("*********************************DONE RECEIVED METRICS***************************");
+        fullMetrics.stream()
+            .sorted()
+            .forEach(System.out::println);
 
-        boolean missingKpis = false;
-        try {
-            PrintWriter writer = new PrintWriter("missing_kpis", "UTF-8");
-            for(String metric : kpis) {
-                if(!metric.isEmpty() && !receivedMetrics.contains(metric)) {
-                    missingKpis = true;
-                    System.out.println("MISSING KPI: " + metric);
-                    writer.println("MISSING KPI: " + metric);
-                }
-            }
-            writer.close();
-        } catch(IOException e) {
-            e.printStackTrace();
-        }
-        if(missingKpis) {
-            System.out.println("THERE ARE MISSING KPIS!");
-            System.exit(1);
-        } else {
+        List<String> missingKpis = readMetrics().stream()
+            .filter(m -> !m.isEmpty())
+            .filter(m -> !receivedMetrics.contains(m))
+            .collect(toList());
+
+        if(missingKpis.isEmpty()) {
             System.out.println("There are no missing KPI's.  yay!");
             System.exit(0);
-        }
+        } else {
+            PrintWriter writer = new PrintWriter("missing_kpis", "UTF-8");
 
-    }
+            missingKpis.stream()
+                .map(m -> String.format("MISSING KPI: %s", m))
+                .forEach(m -> {
+                    System.out.println(m);
+                    writer.write(m);
+                });
 
+            writer.close();
 
-    private void createConnection() throws IOException {
-        System.out.println("HOSTNAME: " + HOSTNAME);
-        System.out.println("CF_DEPLOYMENT_NAME: " + CF_DEPLOYMENT_NAME);
-        System.out.println("NOZZLE_PREFIX: " + NOZZLE_PREFIX);
-        System.out.println("RUN_TIME_MINUTES: " + RUN_TIME);
-        System.out.println("POLL_INTERVAL_SECONDS: " + POLL_INTERVAL);
-
-        JMXServiceURL serviceURL = new JMXServiceURL("service:jmx:rmi://" + HOSTNAME + ":44445/jndi/rmi://" + HOSTNAME + ":44444/jmxrmi");
-        String[] creds = new String[]{USERNAME, PASSWORD};
-        Map<String, String[]> env = new HashMap<>();
-        env.put(JMXConnector.CREDENTIALS, creds);
-        jmxConnector = JMXConnectorFactory.connect(serviceURL, env);
-    }
-
-    private void closeConnection() throws IOException {
-        if(jmxConnector != null) {
-            jmxConnector.close();
+            System.out.println("THERE ARE MISSING KPIS");
+            System.exit(1);
         }
     }
 
-    private Set<String> readMetrics() {
-        String fileName = "kpis.txt";
-        Set<String> metrics = new HashSet<>();
-        //read file into stream, try-with-resources
-        try(Stream<String> stream = Files.lines(Paths.get(fileName))) {
-
-            stream.forEach(metrics::add);
-
-        } catch(IOException e) {
-            e.printStackTrace();
-        }
-        return metrics;
+    private static List<String> readMetrics() throws IOException {
+        return Files.lines(Paths.get(KPI_FILE_NAME))
+            .collect(toList());
     }
 }
