ceb1d88da5b77a7d7b1fed5f5c6eef09a2faf60d
Greg Eng <geng@pivotal.io>
2017-05-02 16:07:06 -0600

    remove unused imports
    Signed-off-by: Jeanette Booher <jbooher-lasell@pivotal.io>

--- src/test/java/io/pivotal/platformMonitoring/kpiValidator/ValidatorTest.java
+++ src/test/java/io/pivotal/platformMonitoring/kpiValidator/ValidatorTest.java
@@ -1,21 +1,13 @@
 package io.pivotal.platformMonitoring.kpiValidator;
 
-import org.cloudfoundry.doppler.*;
-import org.cloudfoundry.reactor.DefaultConnectionContext;
-import org.cloudfoundry.reactor.doppler.ReactorDopplerClient;
-import org.cloudfoundry.reactor.tokenprovider.PasswordGrantTokenProvider;
 import org.junit.Before;
 import org.junit.Test;
-import reactor.core.publisher.Flux;
 
-import javax.management.MBeanServer;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
-import java.util.UUID;
 
 import static io.pivotal.platformMonitoring.kpiValidator.Validator.KPI_FILE_NAME;
 import static java.util.stream.Collectors.toList;
