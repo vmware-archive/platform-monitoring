db2dce577844173794fa34e259aaa4698bd68620
Glenn Oppegard <goppegard@pivotal.io>
2019-04-24 12:04:06 -0600

    Make CF_DEPLOYMENT_NAME configurable via env var
    [#165189109]

--- runValidator.sh
+++ runValidator.sh
@@ -2,9 +2,9 @@
 set -ex
 
 java \
-     -DCF_DEPLOYMENT_NAME=cf \
-     -DCF_API=api.$SYS_DOMAIN \
-     -DCF_USERNAME=$CF_USERNAME \
-     -DCF_PASSWORD=$CF_PASSWORD \
-     -DRUN_TIME_MINUTES=$RUN_TIME_MINUTES \
+     -DCF_DEPLOYMENT_NAME=${CF_DEPLOYMENT_NAME} \
+     -DCF_API=${CF_API} \
+     -DCF_USERNAME=${CF_ADMIN_USERNAME} \
+     -DCF_PASSWORD=${CF_ADMIN_PASSWORD} \
+     -DRUN_TIME_MINUTES=${RUN_TIME_MINUTES} \
      -jar build/libs/platform-monitoring-validator-1.1-jar-with-dependencies.jar
